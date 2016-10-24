33c195613c0c5ac20a2b2023addb107f4e1672bc
Perale Thomas <thomas.perale@openmailbox.org>
2016-10-24 16:34:08 +0200

    Where the magic happen.

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+var exphbs = require('express-handlebars'); 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' })); 
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+app.set('port', process.env.PORT || 3001);
+
+const FOLDER_ENTRY_POINT = './flavours/';
+const COMPILATION_FILE = 'server.js';
+
+app.get('/', (req, res) => {
+    // Listing languages
+    fs.readdir(FOLDER_ENTRY_POINT, (err, files) => {
+        // Picking a language
+       const language = files[Math.floor(Math.random() * files.length)];
+       fs.readdir(FOLDER_ENTRY_POINT + language, (err, files) => {
+            // Picking a framework
+            const framework = files[Math.floor(Math.random() * files.length)];
+            const render = require(FOLDER_ENTRY_POINT + language + '/' + framework + '/' + COMPILATION_FILE);
+            render((js) => {
+                res.render('index', {
+                    js: js,
+                });
+            });
+       });
+    });
+
+});
+
+app.listen(app.get('port'), function () {
+    console.log("Express started on port : " + app.get('port') + " press Ctrl + C to stop.");
+});
